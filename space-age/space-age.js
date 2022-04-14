6fa4a072c69ee95f9aa703f52aaa83fafa94eaa9
Krzysztof Bienias <krzysztofzbienias@gmail.com>
2022-04-14 21:42:58 +0200

    Exercism ✌

---
+++ space-age/space-age.js
@@ -0,0 +1,20 @@
+export const age = (planet, seconds) => {
+  let years = seconds / yearInSeconds;
+  
+  return +(years / PLANETS[planet]).toFixed(2);
+};
+
+/* VARIABLES */
+
+const PLANETS = {
+  'mercury': 0.2408467,
+  'venus': 0.61519726,
+  'earth': 1.0,
+  'mars': 1.8808158,
+  'jupiter': 11.862615,
+  'saturn': 29.447498,
+  'uranus':  84.016846,
+  'neptune': 164.79132
+};
+
+const yearInSeconds = 31557600;
