022ba5dc105aeeeff08dc33809f5da28da8bf45f
Krzysztof Bienias <krzysztofzbienias@gmail.com>
2022-04-14 21:41:38 +0200

    Exercism ✌

---
+++ proverb/proverb.js
@@ -0,0 +1,22 @@
+//
+// This is only a SKELETON file for the 'Proverb' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const proverb = (...arr) => {
+  if (!arr.length) return '';
+  let qualifier = ''; 
+
+  
+  if (typeof arr[arr.length - 1] == 'object') {
+    qualifier = arr[arr.length - 1].qualifier + ' ';
+    arr.pop();
+  }
+
+
+  return arr.map((val, idx, arr) => {
+    if (idx != arr.length - 1) return `For want of a ${val} the ${arr[idx + 1]} was lost.`
+    
+    return `And all for the want of a ${qualifier}${arr[0]}.`
+  }).join('\n');
+};
