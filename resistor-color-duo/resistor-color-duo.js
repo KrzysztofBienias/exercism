3cb290cf86c7bbc853e8d6a872600e20f2b39461
Krzysztof Bienias <krzysztofzbienias@gmail.com>
2022-04-14 21:42:03 +0200

    Exercism ✌

---
+++ resistor-color-duo/resistor-color-duo.js
@@ -0,0 +1,21 @@
+//
+// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const decodedValue = (object) => {
+  return COLORS.indexOf(object[0]) * 10 + COLORS.indexOf(object[1]);
+};
+
+const COLORS = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white',
+];
