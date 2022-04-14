02c3832af7b8235edd652daf6a6aa0006aad7dee
Krzysztof Bienias <krzysztofzbienias@gmail.com>
2022-04-14 21:42:20 +0200

    Exercism ✌

---
+++ roman-numerals/roman-numerals.js
@@ -0,0 +1,32 @@
+//
+// This is only a SKELETON file for the 'Roman Numerals' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const toRoman = (num) => {
+  let str = '';
+
+  for (let i of Object.keys(roman)) {
+    var q = Math.floor(num / roman[i]);
+    num -= q * roman[i];
+    str += i.repeat(q);
+  }
+
+  return str;
+};
+
+  const roman = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1
+  };
